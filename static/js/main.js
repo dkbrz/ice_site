1494d99ab31df537a27a06a77501e117cae34644
Stoneberry <anastasiya.kost98@gmail.ru>
2020-12-19 21:41:57 +0300

    «updated»

--- static/js/main.js
+++ static/js/main.js
@@ -12,17 +12,18 @@ $(document).ready(
         $("#caret-sigh").attr("class", "bi-caret-down");
         $("path", this).attr("d", 
           "M3.204 5L8 10.481 12.796 5H3.204zm-.753.659l4.796 5.48a1 1 0 0 0 1.506 0l4.796-5.48c.566-.647.106-1.659-.753-1.659H3.204a1 1 0 0 0-.753 1.659z");
-        console.log("caret-down")
+        console.log("caret-down");
       } else if ($("#caret-sigh").hasClass("bi-caret-down")) {
           $("#caret-sigh").attr("class", "bi-caret-up");
           $("path", this).attr("d", 
             "M3.204 11L8 5.519 12.796 11H3.204zm-.753-.659l4.796-5.48a1 1 0 0 1 1.506 0l4.796 5.48c.566.647.106 1.659-.753 1.659H3.204a1 1 0 0 1-.753-1.659z");
-          console.log("caret-up")}
+          console.log("caret-up");}
     });
 
 
     let condition1 = (/\/formula\//.test(this.location.pathname));
     let condition2 = ($( ".formula-example" ).length <= 0);
+    let condition3 = (window.location.pathname == '/');
 
     // Если это страница контекста формулы и нет никаких данных, вставить предупреждалку с ошибкой
     if (condition1 && condition2) {
@@ -30,40 +31,53 @@ $(document).ready(
     }
 
 
-    // Если нажали на главную страницу, открываем ее
-    $("#main-page").click( function() {
-      window.location.replace("/");
-    });
+    if (condition3) {
+      preventPageReboot();
+    } else {
+      getFormItems();
+    }
 
 
     // Если на старнице с выдачей контестов формул нажали на знак вернуться к началу страницы
     $(".caret-up-btn").click( function() {
       window.location.replace("#");
     });
-
-
-    // Если нажали на поисковую страницу, открываем ее
-    $(".to-search-btn").click( function() {
-      window.location.replace("/search");
-    });
-
-
-    let sliderEntry = new SearchSlider("#entry-range", "#n-entries-min", "#n-entries-max");
-    let sliderText = new SearchSlider("#text-range", "#n-texts-min", "#n-texts-max");
-
-
-    // Слайдер для частотности конструкции
-    $( "#entry-slider-range" ).slider( sliderEntry );
-
-    // Слайдер для частотности текстов
-    $( "#text-slider-range" ).slider( sliderText );
   
-
-    // Перехват поисковых запросов
-    $("form#search-form").submit( catchSearchQuery );
-
   }
 );
+
+
+
+// Предотвращает перезагрузку страницы
+function preventPageReboot(){
+  $(".to-search-btn").click( function(event) {
+    event.preventDefault();
+    $('main').load('/search' + ' #search', "", getFormItems);
+  });
+
+  $("#main-page").click( function() {
+    event.preventDefault();
+    $('main').load('/' + ' #main-page-content', "", getFormItems);
+  });
+}
+
+
+// Вставляет слайдеры в поисковой блок
+function getFormItems() {
+
+  let sliderEntry = new SearchSlider("#entry-range", "#n-entries-min", "#n-entries-max");
+  let sliderText = new SearchSlider("#text-range", "#n-texts-min", "#n-texts-max");
+
+  // Слайдер для частотности конструкции
+  $( "#entry-slider-range" ).slider( sliderEntry );
+
+  // Слайдер для частотности текстов
+  $( "#text-slider-range" ).slider( sliderText );
+
+  // Перехват поисковых запросов
+  $("form#search-form").submit( catchSearchQuery );
+
+}
 
 
 // Если на старнице с выдачей контестов формул нажали на знак вернуться к началу страницы
@@ -76,10 +90,10 @@ function insertEmptyOutputPlaque() {
       <div class="card-body">
         <h5 class="card-title">Примеров на конструкцию не найдено.</h5>
           <p class="card-text">Попробуйте, пожалуйста, другой запрос.</p>
-          <a class="btn text-light to-search-btn" role="button">Вернуться к поиску</a>
+          <a class="btn text-light" href="/"  role="button">Вернуться на главную</a>
       </div>
       </div>
-    `
+    `;
   $( "#formula-examples" ).append(newDiv);
 }
 
@@ -89,7 +103,7 @@ function SearchSlider (labelName, minName, maxName) {
   this.range = true;
   this.min = 1;
   this.max = 8;
-  this.step =1;
+  this.step = 1;
   this.values = [ 3, 6 ];
   this.create = function() {
       $(labelName).val(" c 3 по 6");
@@ -109,9 +123,10 @@ function SearchSlider (labelName, minName, maxName) {
 // Перехват поисковых запросов
 function catchSearchQuery() {
 
-  let query_values = $('form#search-form').serialize();
+    let query_values = $('form#search-form').serialize();
 
     $.get("api/formula_search?" + query_values, function(data) {
+
       let alpRow = $( "#alphabet-table > tbody > tr" );
 
       alpRow.empty();
@@ -127,7 +142,7 @@ function catchSearchQuery() {
 
 // Группируем данные по первой букве
 function getFirstLetters(acc, item) { 
-  let firstLetter = item['text'].slice(0, 1);
+  let firstLetter = item.verb_text.slice(0, 1);
   if (!acc.hasOwnProperty(firstLetter)) {
          acc[firstLetter] = [];
        }
@@ -137,15 +152,15 @@ function getFirstLetters(acc, item) {
 
 
 // Формируем алфавитные списки
-function insertAlphabet(value, index, array){
+function insertAlphabet(value){
 
   let key = value[0].toUpperCase();
   let constructions = value[1];
-  let newLetter = '<th scope="col"><a href="#' + key + '" style="color:#737a71;">' + key + "</a></th>";
+  let newLetter = '<div class="p-2 bd-highlight"><a href="#' + key + '" style="color:#737a71;">' + key + '</a></div>';
   let examplePlace = '<div class="container mb-5 constructions" id="';
   examplePlace += key + '"><h4 class="main-letter">' + key + '</h4></div>';
     
-  $("#alphabet-table > tbody > tr").append(newLetter);
+  $("#alphabet-table").append(newLetter);
   $("#construction-examples").append(examplePlace);
 
   constructions.forEach(makeConstrList, key);
@@ -156,9 +171,9 @@ function insertAlphabet(value, index, array){
 function makeConstrList(item){
  // [{}, {}, {}, {}]
   let key = this.valueOf();
-  let link = "formula/" + item['id'];
+  let link = "formula/" + item.id;
   let constr = '<p lang="is"><a class="text-secondary" href="';
   constr += link + '" target="_blank">';
-  constr += item['text'] + "</a></p>";
+  constr += item.text + "</a></p>";
   $("#" + key).append(constr);
 }
